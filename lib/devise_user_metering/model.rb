6b2fbc5d9683ba54e7281adc2f44f670ee7055aa
Tenji Tembo <tenji240@gmail.com>
2015-09-03 19:15:21 -0700

    added some comments

--- lib/devise_user_metering/model.rb
+++ lib/devise_user_metering/model.rb
@@ -3,21 +3,25 @@
 module Devise
   module Models
     module UserMetering
-      # This function returns a decimal between 0 and 1 that reflects the amount of the month this user has been 'active'
+      
+      #this function is used to calculate the standard activity for the month of the time passed in
       def active_proportion_of_month(time)
         active_proportion(time, time.beginning_of_month, time.end_of_month)
       end
       
+      #this function is used to calculate the standard activity for the custom interval passed in
       def active_proportion_of_interval(time, time_start, time_end)
         active_proportion(time, time_start, time_end)
       end
 
+      #activates the user to indicate the start of metering
       def activate!
         self.activated_at = Time.new
         self.active = true
         self.save!
       end
 
+      #deactivates the user to indicate the end of metering
       def deactivate!
         now = Time.new
         self.deactivated_at = now
@@ -26,6 +30,7 @@ module Devise
         self.save!
       end
 
+      #indicates the user has been accounted for said month/interval and resets the rollover_active_duration to zero
       def billed!
         self.rollover_active_duration = 0
         self.save!
@@ -33,6 +38,7 @@ module Devise
       
       private
       
+      # This function returns a decimal between 0 and 1 that reflects the amount of the month this user has been 'active'
       def active_proportion(time, month, end_month)
         if end_month > Time.now
           raise StandardError.new("You can't get meter data for incomplete months")
