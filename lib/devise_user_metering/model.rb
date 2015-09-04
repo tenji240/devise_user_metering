27d9e3ecd33c91fe24abf7c6743f6d9633c1a4bd
Dosty Everts <dosty@mustwin.com>
2015-09-03 22:26:43 -0500

    Updated variable names for 'active_proportion_of_interval'

--- lib/devise_user_metering/model.rb
+++ lib/devise_user_metering/model.rb
@@ -4,14 +4,33 @@ module Devise
   module Models
     module UserMetering
       
-      #this function is used to calculate the standard activity for the month of the time passed in
+      # takes a time
+      # returns a decimal between 0 and 1 that reflects the proportion of time in the given month
+      # that the user has been 'active'
       def active_proportion_of_month(time)
         active_proportion(time, time.beginning_of_month, time.end_of_month)
       end
       
       #this function is used to calculate the standard activity for the custom interval passed in
-      def active_proportion_of_interval(time, time_start, time_end)
-        active_proportion(time, time_start, time_end)
+      def active_proportion_of_interval(time, interval_start, interval_end)
+        if interval_end > Time.now
+          raise StandardError.new("You can't get meter data for partial intervals")
+        end
+        if interval_start < self.activated_at
+          raise StandardError.new('No usage data retained for this period of time')
+        end
+
+        in_interval = ->(time) { (interval_start..interval_end).cover?(time) }
+        if in_interval.call(self.activated_at) || in_interval.call(self.deactivated_at)
+          if !active && self.deactivated_at < interval_start
+            return 0
+          end
+          interval_duration = interval_end - interval_start
+          remainder = self.active ? [interval_end - self.activated_at, 0].max : 0
+          (remainder + self.rollover_active_duration) / interval_duration
+        else
+          self.active ? 1 : 0
+        end 
       end
 
       #activates the user to indicate the start of metering
@@ -34,30 +53,6 @@ module Devise
       def billed!
         self.rollover_active_duration = 0
         self.save!
-      end
-      
-      private
-      
-      # This function returns a decimal between 0 and 1 that reflects the amount of the month this user has been 'active'
-      def active_proportion(time, month, end_month)
-        if end_month > Time.now
-          raise StandardError.new("You can't get meter data for incomplete months")
-        end
-        if month < self.activated_at.beginning_of_month
-          raise StandardError.new("No usage data retained for that month")
-        end
-
-        in_month = ->(time) { (month..end_month).cover?(time) }
-        if in_month.call(self.activated_at) || in_month.call(self.deactivated_at)
-          if !active && self.deactivated_at < month
-            return 0
-          end
-          month_duration = end_month - month
-          remainder = self.active ? [end_month - self.activated_at, 0].max : 0
-          (remainder + self.rollover_active_duration) / month_duration
-        else
-          self.active ? 1 : 0
-        end        
       end
       
     end
